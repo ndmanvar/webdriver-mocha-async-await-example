40acd0cd844e81ab46cd208191b33e2bae89c3b9
ndmanvar <ndmanvar@yahoo.com>
2016-10-26 20:58:28 -0700

    backing up

--- src/lib/DriverBuilder.js
+++ src/lib/DriverBuilder.js
@@ -1,13 +1,27 @@
 'use strict';
 
 import webdriver from 'selenium-webdriver';
+import SauceLabs from 'saucelabs';
 
 export default class DriverBuilder {
   constructor() {
-    const builder = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub');
-    const capabilities = webdriver.Capabilities['chrome']();
-    builder.withCapabilities(capabilities);
+    const username = process.env.SAUCE_USERNAME,
+          accessKey = process.env.SAUCE_ACCESS_KEY,
+          saucelabs = new SauceLabs({
+              username: username,
+              password: accessKey
+          }),
+          builder = new webdriver.Builder().usingServer('https://' + username + ':' +accessKey + '@ondemand.saucelabs.com:443/wd/hub');
+
+    builder.withCapabilities({
+        'browserName': 'chrome',
+        'platform': 'Windows 7',
+        'version': '43.0',
+        'username': username,
+        'accessKey': accessKey
+    });
     this._driver = builder.build();
+    this.saucelabs = saucelabs;
   }
 
   get driver() {
@@ -15,6 +29,12 @@ export default class DriverBuilder {
   }
 
   async quit() {
+    const sessionId = (await this._driver.getSession()).id_;
+
+    await this.saucelabs.updateJob(sessionId, {
+      passed: false,
+      name: '444'
+    });
     return this._driver.quit();
   }
 
